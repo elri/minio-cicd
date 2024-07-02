c78b102858ae96e03bb8382a02c37889a62127b2
ERI <eri@cparta.se>
2024-07-02 16:49:21 +0200

    rm entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 ARG VERSION=latest
 FROM minio/minio:$VERSION
 
-ENTRYPOINT [ "docker-entrypoint.sh" ]
-
 CMD ["minio", "server", "/data", "--console-address", ":9001"]
--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-if [ -z $MINIO_BUCKET_NAME ]; then
-/usr/bin/mc alias set minio http://localhost:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD;
-/usr/bin/mc mb minio/$MINIO_BUCKET_NAME;
-if [ $MINIO_BUCKET_PUBLIC ]
-/usr/bin/mc policy set public minio/$MINIO_BUCKET_NAME;
-fi
-fi
